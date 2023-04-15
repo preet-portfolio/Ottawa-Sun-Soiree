12c31703dce867fb6a7e22fa9adef76625bacd66
Preet Panchal <preet2020@icloud.com>
2023-04-14 23:16:05 -0400

    main framework

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div className="mx-auto bg-blue-200">
+      <div>Home Page</div>
+    </div>
+  );
+};
+
+export default index;
