cd07417290432ea57312798703727ceccf8016d0
Preet Panchal <preet2020@icloud.com>
2023-04-20 13:02:58 -0400

    main

--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -1,5 +1,8 @@
 import { Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
+import { Menu } from "@headlessui/react";
+import { HomeIcon, Bars2Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { motion } from "framer-motion";
+import React from "react";
 
 const links = [
   { href: "/", label: "Home" },
@@ -15,23 +18,54 @@ const links = [
 ];
 
 const NavigationMenu = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <Menu as="nav" className="flex space-x-4 m-2 text-lg">
-      {links.map((link) => (
-        <Menu.Item key={link.href}>
-          {({ active }) => (
-            <a
-              href={link.href}
-              className={`${
-                active ? "text-gray-900" : "text-gray-500"
-              } hover:text-gray-900`}
-            >
-              {link.label}
-            </a>
+    <nav className="flex items-center justify-between m-2 md:px-10">
+      <a href="/" className="text-lg font-bold text-gray-900">
+        Logo
+      </a>
+      <div className="flex md:hidden">
+        <button
+          onClick={toggleMenu}
+          type="button"
+          className="text-gray-900 hover:text-gray-500 focus:outline-none"
+        >
+          {isOpen ? (
+            <XMarkIcon className="w-6 h-6" />
+          ) : (
+            <Bars2Icon className="w-6 h-6" />
           )}
-        </Menu.Item>
-      ))}
-    </Menu>
+        </button>
+      </div>
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: isOpen ? 1 : 0, y: isOpen ? 0 : -20 }}
+        transition={{ duration: 0.3 }}
+        className={`absolute z-20 top-full right-0 md:relative md:flex md:items-center md:space-x-4 md:mt-0 md:mr-2 md:flex-row ${
+          isOpen ? "block" : "hidden"
+        }`}
+      >
+        {links.map((link) => (
+          <Menu.Item key={link.href}>
+            {({ active }) => (
+              <a
+                href={link.href}
+                className={`${
+                  active ? "text-gray-900" : "text-gray-500"
+                } block px-3 py-2 text-lg font-medium text-center hover:text-gray-900 md:text-left`}
+              >
+                {link.label}
+              </a>
+            )}
+          </Menu.Item>
+        ))}
+      </motion.div>
+    </nav>
   );
 };
 
